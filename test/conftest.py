6ab0c58a1d396fbd2d3cee36d6aa47b015f5b13f
joao tiago viegas <3536754+jtviegas@users.noreply.github.com>
2025-03-28 11:30:53 +0100

    added safety dependency scan on pipeline and updated bashutils

--- test/conftest.py
+++ test/conftest.py
@@ -1,3 +1,19 @@
+"""
+Module Name: 
+Description: test configurations
+Author:
+Date:
+Version:
+
+Dependencies:
+- ...
+
+Environment Variables:
+- ...
+
+Usage:
+    >>> ...
+"""
 import os
 import sys
 import tempfile
@@ -18,6 +34,17 @@ def assert_frames_are_equal(
     sort_columns: List[str],
     abs_tol: float = None,
 ):
+    """check pandas dataframe equality
+
+    Args:
+        arg (type): description
+
+    Returns:
+        None
+
+    Raises:
+        AssertionError if not equal
+    """
     results_sorted = actual.sort_values(by=sort_columns).reset_index(drop=True)
     expected_sorted = expected.sort_values(by=sort_columns).reset_index(drop=True)
     if abs_tol is not None:
@@ -41,11 +68,34 @@ def assert_frames_are_equal(
 
 @pytest.fixture(scope="session")
 def resources_folder() -> str:
+    """provides the location of test respource folder
+
+    Args:
+        arg (type): description
+
+    Returns:
+        None
+
+    Raises:
+        ...
+    """
     return os.path.abspath(os.path.join(os.path.dirname(__file__), "resources"))
 
 
 @pytest.fixture(scope="session")
 def temporary_folder() -> str:
+    """provides a temporary folder
+
+    Args:
+        arg (type): description
+
+    Returns:
+        None
+
+    Raises:
+        ...
+    """
+    # pylint: disable=consider-using-with
     _folder = tempfile.TemporaryDirectory("+wb").name
     if not os.path.exists(_folder):
         os.makedirs(_folder)
